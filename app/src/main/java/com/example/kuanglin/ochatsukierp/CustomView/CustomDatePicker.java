57563b0ec517b0be6d52ac14877551432efd80e9
林廣 <slk11075@gmail.com>
2017-03-18 11:06:07 +0800

    Start: project Start

---
+++ app/src/main/java/com/example/kuanglin/ochatsukierp/CustomView/CustomDatePicker.java
@@ -0,0 +1,10 @@
+package com.example.kuanglin.ochatsukierp.CustomView;
+
+import android.widget.RelativeLayout;
+
+/**
+ * Created by KuangLin on 2017/3/17.
+ */
+
+public class CustomDatePicker extends RelativeLayout {
+}
